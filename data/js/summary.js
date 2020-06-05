f5af786dec366d89b5ed0b0371ea6f88c35456ed
yammadev <yefferson.marin@gmail.com>
2020-06-04 22:52:02 -0500

    Data updated 04-06-2020 22:27

--- data/js/summary.js
+++ data/js/summary.js
@@ -1,22 +1,22 @@
 window.summary = {
   "0":{
     "VALUE":"CASOS",
-    "TOTAL":33354
+    "TOTAL":35120
   },
   "1":{
     "VALUE":"RECUPERADOS",
-    "TOTAL":12288
+    "TOTAL":12921
   },
   "2":{
     "VALUE":"FALLECIDOS",
-    "TOTAL":1045
+    "TOTAL":1087
   },
   "3":{
     "VALUE":"ULTIMO REPORTE",
-    "TOTAL":1521
+    "TOTAL":1766
   },
   "4":{
     "VALUE":"ANTERIOR REPORTE",
-    "TOTAL":1340
+    "TOTAL":1521
   }
 }
